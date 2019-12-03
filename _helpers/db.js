e2db96af440333f49124407d2ed4711de738fb0b
rawda95 <rawda95.r@gmail.com>
2019-12-03 10:07:03 +0200

    Update db.js
    remove local db connection

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //local connect 
-mongoose.connect('mongodb://127.0.0.1:27017/Contacts', { useNewUrlParser: true, useFindAndModify: false });
+//mongoose.connect('mongodb://127.0.0.1:27017/Contacts', { useNewUrlParser: true, useFindAndModify: false });
 
 
 // remote onnect 
